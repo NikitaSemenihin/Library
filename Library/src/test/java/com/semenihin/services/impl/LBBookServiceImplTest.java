c0f545eb678f1088d1a660f87a4647844d5f9a3d
Nikita <semenihin.rink@gmail.com>
2025-05-30 19:24:44 +0300

    adding maven and junit

---
+++ Library/src/test/java/com/semenihin/services/impl/LBBookServiceImplTest.java
@@ -0,0 +1,48 @@
+package com.semenihin.services.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LBBookServiceImplTest {
+
+    @Test
+    void createBook() {
+    }
+
+    @Test
+    void updateBook() {
+    }
+
+    @Test
+    void getBooks() {
+    }
+
+    @Test
+    void findBook() {
+    }
+
+    @Test
+    void deleteBook() {
+    }
+
+    @Test
+    void printBooks() {
+    }
+
+    @Test
+    void rentBook() {
+    }
+
+    @Test
+    void returnBook() {
+    }
+
+    @Test
+    void updateUserInBook() {
+    }
+
+    @Test
+    void exist() {
+    }
+}
